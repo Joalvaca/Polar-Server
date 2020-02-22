981023e8650bffc966393171f39a3ee3bce8b5e2
joel Alva <panthersjoe@hotmail.com>
2020-02-21 23:56:08 -0500

    debug

---
+++ test/footprints-endpoint.spec.js
@@ -0,0 +1,13 @@
+const { expect } = require("chai");
+const knex = require("knex");
+const app = require("../src/app");
+
+describe("Footprints Endpoints", function() {
+
+    let db 
+    
+    const
+
+
+
+});
