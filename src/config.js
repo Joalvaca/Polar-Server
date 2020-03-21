c95440e241eb095b5f26b5fb153a53ee9efbd32c
joel Alva <panthersjoe@hotmail.com>
2020-03-21 17:47:40 -0400

    changed DB

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://polar:1@localhost/footprints",
-  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret"
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://polar:1@localhost/footprints",
+  JWT_SECRET: process.env.JWT_SECRET || "secret",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://polar:1@localhost/footprints-test"
 };
